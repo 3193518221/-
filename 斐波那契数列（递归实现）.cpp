93159a1e608bad2afb55104d03acb10d9aacda3a
3193518221 <102988213+3193518221@users.noreply.github.com>
2025-11-12 20:35:11 +0800

    Create 斐波那契数列（递归实现）.cpp

---
+++ 斐波那契数列（递归实现）.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <string.h>
+#include<math.h>
+#include<stdlib.h>
+int fb(int n)
+{
+	if (n <= 2)
+		return 1;
+	if(n>2)
+		return fb(n - 1) + fb(n - 2);
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	fb(n);
+	printf("%d", fb(n));
+	return 0;
+}
